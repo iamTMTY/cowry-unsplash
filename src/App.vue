0b33844e71e45de6e4d4deaba82b759defbde763
temi-tayo-s <temitayo@bepeerless.co>
2024-09-29 16:04:32 +0100

    added infinite scroll

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,16 @@ import Skeleton from 'components/Skeleton.vue'
 import ImageModal from 'components/ImageModal.vue'
 import { ImageType } from 'types';
 import api from 'utilities/api';
-import {ref, onBeforeMount} from 'vue'
+import {ref, onBeforeMount, watch} from 'vue'
+import { useScroll } from '@vueuse/core'
 
   const PAGE_SIZE = 20
-  const SKELETON_COUNT = new Array(PAGE_SIZE).fill('skeleton')
-  const images = ref<Array<ImageType>>([])
-  const isLoading = ref(true)
+  const SKELETONS = new Array(PAGE_SIZE).fill('skeleton')
+  const images = ref<Array<ImageType | 'skeleton'>>([])
   const modalData = ref<ImageType>()
   const search = ref('')
+  const page = ref(1)
+  const totalPages = ref(0)
 
   const showModal = (image: ImageType) => {
     modalData.value = image
@@ -19,19 +21,43 @@ import {ref, onBeforeMount} from 'vue'
 
   const getImages = async (query?: string) => {
     try {
-      isLoading.value = true
-      const url = query ? `/search/photos?query=${query}&page=1&per_page=${PAGE_SIZE}` : `/photos?page=1&per_page=${PAGE_SIZE}`
+      images.value = images.value.concat(SKELETONS)
+      const url = query ? `/search/photos?query=${query}&page=${page.value}&per_page=${PAGE_SIZE}` : `/photos?page=${page.value}&per_page=${PAGE_SIZE}`
       let res = await api.get(url)
       if(res.status === 200) {
-        images.value = res.data?.results || res.data
+        const data = res.data?.results || res?.data || []
+        const end = (page.value - 1) * PAGE_SIZE
+        images.value = [...images.value.slice(0, end), ...data]
+        totalPages.value = !!search ? res.data?.results?.total_pages || 0 : 0
       }
-      isLoading.value = false
     } catch (error) {
       
     }
   } 
 
+  const { arrivedState } = useScroll(document, {
+  throttle: 100,
+  behavior: 'smooth',
+  offset: {
+    bottom: 100
+  }
+})
+
+watch(arrivedState, (state) => {
+  if(state.bottom && (!totalPages.value || page.value < totalPages.value)) {
+    page.value = page.value + 1
+    getImages(search.value)
+  }
+})
+
+  function resetList() {
+    page.value = 1
+    totalPages.value = 0
+    images.value = []
+  }
+
   const setSearch = (query?: string) => {
+    resetList()
     search.value = query || ''
     getImages(query)
   }
@@ -43,12 +69,16 @@ import {ref, onBeforeMount} from 'vue'
     }
   }
 
+  const getIsLoading = (data: ImageType | 'skeleton') => {
+    return data === 'skeleton'
+  }
+
   onBeforeMount(getImages)
 
 </script>
 
 <template>
-  <div class="main">
+  <div class="main" >
     <div class="main_content">
       <div class="search_result_component" v-if="!!search">
         <div class="back_component" :onclick="() => setSearch()"><img src="./assets/back.svg" class="back_icon" alt="back" /></div>
@@ -59,11 +89,9 @@ import {ref, onBeforeMount} from 'vue'
         <input type="text" class="search_input" placeholder="search for photo" :onkeypress="handleSearch" />
       </div>
       <div class="grid">
-        <template v-for="(_s,_i) in SKELETON_COUNT" :key="'skeleton'+_i" v-if="isLoading">
-          <Skeleton />
-        </template>
-        <template v-for="image in images" :key="image.id" v-if="!isLoading">
-          <ImageCard :data="image" :open="() => showModal(image)"/>
+        <template v-for="(image, _i) in images" :key="'image' + _i">
+          <Skeleton v-if="getIsLoading(image)" />
+          <ImageCard v-else="getIsLoading(image)" :data="image" :open="() => showModal(image)"/>
         </template>
       </div>
     </div>
@@ -73,7 +101,7 @@ import {ref, onBeforeMount} from 'vue'
 
 <style scoped>
 .main {
-  min-height: calc(100vh - 0px);
+  /* min-height: calc(100vh - 0px); */
   background: -webkit-linear-gradient(top, #DDE2E8 300px, white 0%);
   background: -moz-linear-gradient(top, #DDE2E8 300px, white 0%);
   background: -ms-linear-gradient(top, #DDE2E8 300px, white 0%);
@@ -90,6 +118,7 @@ import {ref, onBeforeMount} from 'vue'
 }
 .search_result_component {
   display: flex;
+  flex-wrap: wrap;
   gap: 16px;
   align-items: center;
   width: 100%;
